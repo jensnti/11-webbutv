8c8e028e411e631851b860bcdd72f6c32099c654
jensnti <jensandreasson77@gmail.com>
2021-05-26 16:11:10 +0200

    :sparkles: meta info

---
+++ src/_data/meta.js
@@ -0,0 +1,8 @@
+Wmodule.exports = {
+    // NOTE: `process.env.URL` is provided by Netlify, and may need
+    // adjusted pending your host
+    url: process.env.URL || "http://localhost:8080",
+    siteName: "Webbutveckling",
+    siteDescription: "",
+    authorName: "Jens Andreasson"
+  };
