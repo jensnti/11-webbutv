37af052fec8ec4a5fb020ee38800be54ca030434
jensnti <jensandreasson77@gmail.com>
2021-05-29 14:38:14 +0200

    💬📄meta licensing

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,8 +1,14 @@
 module.exports = {
     // NOTE: `process.env.URL` is provided by Netlify, and may need
     // adjusted pending your host
-    url: process.env.URL || "http://localhost:8080",
-    siteName: "Webbutveckling",
-    siteDescription: "",
-    authorName: "Jens Andreasson"
-  };
+    url: process.env.URL || 'http://localhost:8080',
+    language: 'sv',
+    siteName: 'Webbutveckling',
+    siteDescription:
+        'Kurssida för webbutveckling. HTML, CSS, javascript, tillgänglighet',
+    author: {
+        name: 'Jens Andreasson',
+        email: 'jensandreasson77@gmail.com',
+        url: 'https://jensa.xyz'
+    }
+};
