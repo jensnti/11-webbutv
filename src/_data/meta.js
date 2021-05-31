369764846e184d3964583ecc175d1373ad457438
jensnti <jensandreasson77@gmail.com>
2021-05-31 19:31:11 +0200

    ➕💄⚗️randomized theme color

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,10 +1,21 @@
+const colors = require('nice-color-palettes');
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+}
+
+const color = colors[getRandomIntInclusive(0, colors.length)];
+// const pick = color[getRandomIntInclusive(0, color.length)];
+
 module.exports = {
     // NOTE: `process.env.URL` is provided by Netlify, and may need
     // adjusted pending your host
     url: process.env.URL || 'http://localhost:8080',
     language: 'sv',
     siteName: 'Webbutveckling 1',
-    themeColor: '#f3722c',
+    themeColor: color[0],
     siteDescription:
         'Kurssida för webbutveckling. HTML, CSS, javascript, tillgänglighet',
     author: {
